6c148d18ae82a8f673aa3ab43bdcf2ed5b2ded58
nitr-am <martin7gomez@hotmail.com>
2019-10-22 21:37:33 +0000

    Implmented basic routes

--- run.py
+++ run.py
@@ -6,8 +6,16 @@ app = Flask(__name__) #This is a new application.
 
 @app.route("/") #This is the app route decorator
 def index():  #This is the funcion that is going to be bound to our decorator.
-    return "<h1>Hello There</h1>"
+    """Main page with instructions"""
+    return "To send a message use: /USERNAME/MESSAGE"
 
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
 
 if __name__ =="__main__":
     app.run(host=os.getenv("IP"),
