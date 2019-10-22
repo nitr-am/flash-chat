af0d4a8d14c953db63d02765967a0a8c15be3eeb
nitr-am <martin7gomez@hotmail.com>
2019-10-22 22:28:18 +0000

    Adding chatting functionality but with an issue on displaying messages

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
 import os #This will give us acces to the enviornment variables
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__) #This is a new application.
+messages = []
+
+def add_messages(username, message):
+    """ Add messages to the messages list """
+    messages.append("{}: {}".format(username, message))
+
+def get_all_messages():
+    """Get all of the messages and separate them with a `br`"""
+    return "<br>".join(messages)
 
 
 @app.route("/") #This is the app route decorator
@@ -11,11 +20,14 @@ def index():  #This is the funcion that is going to be bound to our decorator.
 
 @app.route("/<username>")
 def user(username):
-    return "Hi " + username
+    """ Display username """
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 if __name__ =="__main__":
     app.run(host=os.getenv("IP"),
